8aadf7de78c300b49323e8e5072c200201475af4
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-11-04 10:44:39 +0100

    modifications

---
+++ pages/page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Foodapp is a website to help you. Via the website, you can order your dish and will be delivered directly to your home.">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
